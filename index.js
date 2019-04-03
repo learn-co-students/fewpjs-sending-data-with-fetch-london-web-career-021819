79762d45e680ea809282a91de3f697bd3bf358b7
mariamjaludi <mariam.jaludi@gmail.com>
2019-04-03 16:09:10 +0100

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,25 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+let formData = {
+  "firstName": "Mariam",
+  "registryMessage": "message"
+};
+
+let configObj = {
+  method: "POST",
+  body: JSON.stringify(formData)
+};
+function registerSelf(){
+  fetch("http://guestbook.example.com/register", configObj)
+    .then(function(response) { return response.json()})
+    .then(function(json){ return json.message;})
+}
+
+function errorSelf(){
+  fetch("http://guestbook.example.com/register-error", configObj)
+    .then(function(response) {return response.json()})
+    .then(function(json){return json.message})
+    .catch(function(error){return ":(";})
+}
