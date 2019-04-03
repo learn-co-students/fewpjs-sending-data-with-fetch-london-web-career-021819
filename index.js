2b1194403fc30d0436dffeccbad97c0c11de8b56
khang <khang.le27@gmail.com>
2019-04-03 16:23:47 +0100

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,42 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function registerSelf(){
+
+  let formData = {
+    "firstName": "Khang",
+    "registryMessage": "Test"
+  }
+
+  let configObj = {
+    method: "POST",
+    body: formData
+  }
+
+  return fetch("http://guestbook.example.com/register", configObj)
+  .then(response => response.json())
+  .then(json => json.message)
+}
+
+
+function errorSelf(){
+
+  let formData = {
+    "firstName": "Name invalid bla bla bla",
+    "registryMessage": "Test"
+  }
+
+  let configObj = {
+    method: "POST",
+    body: formData
+  }
+
+  return fetch("http://guestbook.example.com/register-error", configObj)
+  .then(response => response.json())
+  .then(json => json.message)
+  .catch(function(error){
+    alert("error")
+    return ':('
+  })
+}
