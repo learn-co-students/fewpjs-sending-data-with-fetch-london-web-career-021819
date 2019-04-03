102470de3ab9639a21fe2eb5046a03b7ecfc7645
saqemlas <saqemlas@googlemail.com>
2019-04-03 15:05:13 +0100

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-const testVar = {}
+function registerSelf(){
+  return fetch("http://guestbook.example.com/register", {
+    method: "POST",
+    body: {
+      firstName: "Sal",
+      message: "Message" } } )
+    .then(function(response){return response.json()})
+    .then(function(json){return json.message} )
+}
 
-function testFunc() {
-  return "hi"
+
+ function errorSelf(){
+  return fetch('http://guestbook.example.com/register-error', {
+    method: "POST",
+    body: {
+      firstName: "Sal",
+      message: "Message" } } )
+      .then(function(response){return response.json() } )
+      .then(function(json){return json.message} )
+      .catch(function(error){ return ":(" } )
 }
