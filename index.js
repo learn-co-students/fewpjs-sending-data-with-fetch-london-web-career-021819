4e5ac76b76cb28196182f18e5b9efb6b6958f9b6
Alex Bujenita <dekc89@gmail.com>
2019-04-04 09:58:10 +0100

    done

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
-const testVar = {}
+const testVar = {};
 
-function testFunc() {
-  return "hi"
+
+function registerSelf() {
+  const url = "http://guestbook.example.com/register";
+  const data = {
+    firstName: "Alex",
+    registryMessage: "Yo"
+  };
+  
+  
+  const config = {
+    method: "POST",
+    body: JSON.stringify(data)
+  };
+
+  return fetch(url, config)
+    .then(resp => resp.json())
+    .then(js => js.message)
+      .catch(err => console.log(err))
+}
+
+function errorSelf() {
+  const errUrl = "http://guestbook.example.com/register-error";
+
+  const data = {
+    firstName: "Alex",
+    registryMessage: "Yo"
+  };
+  
+  
+  const config = {
+    method: "POST",
+    body: JSON.stringify(data)
+  };
+
+  return fetch(errUrl, config)
+  .then(resp => resp.json())
+  .then(js => js.message)
+    .catch(err => {
+      console.log(err)
+      return ':('
+    })
+
 }
