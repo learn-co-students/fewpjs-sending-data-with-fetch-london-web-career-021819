218e4db33bbc2a71540ca559653cd11101b67142
CalieR <calielrushton@gmail.com>
2019-04-04 08:51:05 +0100

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-const testVar = {}
+function registerSelf() {
+  return fetch("http://guestbook.example.com/register", {
+    method: "POST",
+    body: {
+      firstName: "Calie",
+      message: "Did it work???"
+    }
+  })
+    .then(resp => resp.json())
+    .then(content => {
+      return content.message;
+    });
+}
 
-function testFunc() {
-  return "hi"
+function errorSelf() {
+  return fetch("http://guestbook.example.com/register-error", {
+    method: "POST",
+    body: {
+      firstName: "Calie",
+      message: "Did it work???"
+    }
+  })
+    .then(resp => resp.json())
+    .then(content => {
+      return content.message;
+    })
+    .catch(error => {
+      return ":(";
+    });
 }
