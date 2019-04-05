f25ebf4a66166e46aca9c100a4e15021fa5e02ea
max-powell <github email address>
2019-04-05 12:14:14 +0100

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,30 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function registerSelf () {
+  return fetch('http://guestbook.example.com/register', {
+    method: 'POST',
+    body: JSON.stringify({
+      'firstName': 'max',
+      'registryMessage': 'test'
+    })
+  }).then(function(res) {
+      return res.json()
+  }).then(function(json) {
+      return json.message
+  })
+}
+
+function errorSelf () {
+  return fetch('http://guestbook.example.com/register-error', {
+    method: 'POST',
+    body: JSON.stringify({
+      'firstName': 'max',
+      'registryMessage': 'test'
+    })
+  })
+    .then(res => res.json())
+    .then(json => json.message)
+    .catch(() => ':(')
+}
